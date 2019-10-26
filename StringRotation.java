51a8668ac3bd1e50cc997a9473e5102121f46e76
iamkshitij <iamkshitij@live.com>
2019-10-26 19:48:20 +0530

    Solution Updated

--- StringRotation.java
+++ StringRotation.java
@@ -4,18 +4,19 @@
 public class StringRotation {
 
     public static void main(String[] args) {
-        boolean result = isRotation("waterbottle", "erbottlewat");
-        //boolean result = isRotation("waterbottle", "erbattlewat"); -- false
+       // boolean result = isRotation("waterbottle", "erbottlewat");
+        boolean result = isRotation("waterbottle", "erbattlewat"); 
+    
         System.out.println(result);
     }
 
     public static boolean isRotation(String original, String given){
         String newString = original + original;
-        return isSubstring(newString, given) ? true : false;
+        return isSubstring(newString, given);
     }
 
     // Given in question this method isSubstring is given
     public static boolean isSubstring(String str1, String str2){
-        return str1.contains(str2) ? true : false;
+        return str1.contains(str2);
     }
 }
